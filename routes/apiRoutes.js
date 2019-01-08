7f2ee009a4479dcfae9ee5ce15e06a1fcf4d0ec5
SheetalSrikumar <bs.sheetal@gmail.com>
2019-01-08 14:41:32 -0800

    C:/Program Files/Git/hiking route works

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,33 +5,35 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 var axios = require('axios');
 var keys = require("../keys.js")
 var rei_hiking = keys.hike.id;
+var geocode_key = "AIzaSyBl_HHn6uZU5pUaqN8CWz6ppoYUJbCfTmc";
+var geocode = require('@google/maps');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", isAuthenticated, function(req, res) {
+  app.get("/api/examples", isAuthenticated, function (req, res) {
     db.Example.findAll({
       where: {
         UserId: req.user.id
       }
-    }).then(function(dbExamples) {
+    }).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", isAuthenticated, function(req, res) {
+  app.post("/api/examples", isAuthenticated, function (req, res) {
     db.Example.create({
       UserId: req.user.id,
       text: req.body.text,
       description: req.body.description
-    }).then(function(dbExample) {
+    }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", isAuthenticated, function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  app.delete("/api/examples/:id", isAuthenticated, function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (
       dbExample
     ) {
       res.json(dbExample);
@@ -41,7 +43,7 @@ module.exports = function(app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
@@ -51,33 +53,52 @@ module.exports = function(app) {
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     console.log(req.body);
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(422).json(err.errors[0].message);
       });
   });
 
-  app.get("/hiking",function (req,res) {
-    console.log("secretkey is" + rei_hiking);
-    var hikingqueryurl = "https://www.hikingproject.com/data/get-trails?lat=40.0274&lon=-105.2519&maxDistance=10&key=" + rei_hiking;
-    console.log(hikingqueryurl);
-    axios.get(hikingqueryurl).then(function (response) {
+  app.get("/hiking", function (req, res) {
+    var lat;
+    var long;
+    var googleMapsClient = geocode.createClient({
+      key: geocode_key
+    });
 
-      var JSONDATA = response.data.trails;
-      res.send(JSONDATA);
+    googleMapsClient.geocode({
+      components:
+      {
+        locality: 'Rancho+PenasQuitos',
+        country: 'USA'
+      }
+    }, function (err, response) {
+      if (!err) {
+        lat = response.json.results[0].geometry.location.lat;
+        long = response.json.results[0].geometry.location.lng;
 
-    })
+        var hikingqueryurl = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + long + "&maxDistance=10&key=" + rei_hiking;
+        console.log(hikingqueryurl);
+        axios.get(hikingqueryurl).then(function (response) {
+
+          var JSONDATA = response.data.trails;
+          res.send(JSONDATA);
+
+        })
+      }
+    });
+
   })
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
