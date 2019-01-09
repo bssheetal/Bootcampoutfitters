2ecddb39229919f8a9596a9c8ae7bfd12ab22e4d
Iris Cheng <chengiris66@gmail.com>
2019-01-09 11:18:30 -0800

    touched test.js

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,9 +1,30 @@
 var expect = require("chai").expect;
 
+function multiply(x, y) {
+  if (typeof x !== 'number' || typeof y !== 'number') {
+     
+  } else {
+    return x * y;
+  };
+};
+
 describe("canary test", function() {
   // A "canary" test is one we set up to always pass
   // This can help us ensure our testing suite is set up correctly before writing real tests
   it("should pass this canary test", function() {
     expect(true).to.be.true;
+  });
+});
+describe("Multiply", function() {
+  // A "canary" test is one we set up to always pass
+  // This can help us ensure our testing suite is set up correctly before writing real tests
+  it("should multiply 2 numbers", function() {
+    expect(nultiply(2, 4)).to.equal(8);
+  });
+
+  it("", function() {
+    expect(function() {
+      expect();
+    });
   });
 });
